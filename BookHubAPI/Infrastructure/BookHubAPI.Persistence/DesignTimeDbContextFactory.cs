5ca30516bbbc062aad236a0dd6b673549b13256b
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-09-20 16:28:12 +0300

    Added DesignTimeDbContextFactory
    Added DesignTimeDbContextFactory to support migrations in case of using the CLI. PMC in Visual Studio does not require this as it has access to runtime configurations.

---
+++ BookHubAPI/Infrastructure/BookHubAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using BookHubAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BookHubAPI.Persistence;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BookHubDbContext>
+{
+    public BookHubDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<BookHubDbContext> dbContextOptionsBuilder = new();
+        dbContextOptionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=BookHubDb;");
+        return new(dbContextOptionsBuilder.Options);
+    }
+}
